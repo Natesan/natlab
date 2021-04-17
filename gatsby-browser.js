89c95f2073c983f98eb3935c85cfc3494f493dfb
natesan.sivagnanam <natesan.sivagnanam@sap.com>
2021-04-17 03:50:49 -0400

    Added Notification on Timer Expiry

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,9 @@ import "./src/styles/global.css"
 export const onClientEntry = () => {
   window.onload = () => {
     if (!("Notification" in window)) {
-      console.log("🚀 ~ file: gatsby-browser.js ~ line 20 ~ onClientEntry ~ This browser does not support notifications")
+      console.log(
+        "🚀 ~ file: gatsby-browser.js ~ line 07 ~ onClientEntry ~ This browser does not support notifications"
+      )
     } else {
       let promise = Notification.requestPermission()
       promise.then(handleNotification)
@@ -15,16 +17,24 @@ export const onClientEntry = () => {
       )
     }
 
+    window.handleCountDownExpiration = () => {
+      console.log(
+        "🚀 ~ file: gatsby-browser.js ~ line 21 ~ handleCountDownExpiration ~ expired"
+      )
+      var text = "Hey! Your Count Down Timer has Expired"
+      new Notification("Count Down Timer", { body: text })
+    }
+
     function handleVisibilityChange() {
       if (document.hidden) {
         console.log(
-          "🚀 ~ file: gatsby-browser.js ~ line 11 ~ handleVisibilityChange ~ tabOut()"
+          "🚀 ~ file: gatsby-browser.js ~ line 31 ~ handleVisibilityChange ~ tabOut()"
         )
         document.getElementById("count-down-pause").click()
         document.getElementById("count-up-pause").click()
       } else {
         console.log(
-          "🚀 ~ file: gatsby-browser.js ~ line 13 ~ handleVisibilityChange ~ tabIn()"
+          "🚀 ~ file: gatsby-browser.js ~ line 37 ~ handleVisibilityChange ~ tabIn()"
         )
         document.getElementById("count-down-resume").click()
         document.getElementById("count-up-start").click()
